fd6e81dd00c0ff2f5c2c43ee99f9a901426e9cc4
CaioRen4to <caio.renato011@gmail.com>
2025-11-09 03:55:25 -0300

    Começando o Form-Captura de dados

--- src/app/pages/FormComponent/form-dados-component/form-dados-component.ts
+++ src/app/pages/FormComponent/form-dados-component/form-dados-component.ts
@@ -1,22 +1,48 @@
 import { CommonModule } from '@angular/common';
+import { Router } from '@angular/router';
 import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { Forms } from "../../ComponentCompartilhado/forms/forms";
 
 @Component({
   selector: 'app-form-dados-component',
-  imports: [FormsModule, CommonModule],
+  imports: [FormsModule, CommonModule, Forms],
   templateUrl: './form-dados-component.html',
   styleUrl: './form-dados-component.css'
 })
 export class FormDadosComponent {
+[x: string]: any;
 
-  salario: number | null = null; 
-  despesas: number | null = null;
+  message: string = '';
+  formData: any = {};
   
-SalvaDados(){
-  if(this.salario !== null && this.despesas !== null) { 
+  dadosFields = [ 
+    {
+      name: 'Salario',
+      type: 'Salario',
+      placehoder: 'Digite o seus proventos',
+      label: 'Salario'
+    },
+    {
+      name: 'Despesas',
+      type: 'Despesas',
+      placehoder: 'Digite as suas depesas',
+      label: 'Despesas'
+    }
+  ]
 
-    localStorage.setItem('userSalario', this.salario.toString());
+constructor(private router: Router) {}
+
+  updateFormData(data: any) {
+    this.formData = data;
+    console.log('Dados atualizados:', this.formData);
+  }
+
+
+SalvaDados(){
+  if(this.formData.Salario !== null && this.formData.Despesas !== null) { 
+
+    localStorage.setItem('userSalario', this.formData.Salario.toString());
     
     }
 }
