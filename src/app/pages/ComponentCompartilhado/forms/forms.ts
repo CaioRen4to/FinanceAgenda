aa778180f4fdeaa4cc3bad808e4e1ef045690b39
Gabriel Coutinho <165735404+gabrielcoutinhoDL@users.noreply.github.com>
2025-11-20 03:03:52 -0300

    Inicio da Aside

--- src/app/pages/ComponentCompartilhado/forms/forms.ts
+++ src/app/pages/ComponentCompartilhado/forms/forms.ts
@@ -1,11 +1,12 @@
 import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
+import { Aside } from '../../HomeComponent/aside/aside';
 
 @Component({
   selector: 'app-forms',
   standalone: true,
-  imports: [FormsModule, CommonModule],
+  imports: [FormsModule, CommonModule, Aside],
   templateUrl: './forms.html',
   styleUrl: './forms.css'
 })
--- src/app/pages/HomeComponent/tela-incial-component/tela-incial-component.ts
+++ src/app/pages/HomeComponent/tela-incial-component/tela-incial-component.ts
@@ -3,12 +3,13 @@ import { RouterOutlet } from "@angular/router";
 import { CabecalhoComponent } from '../cabecalho-component/cabecalho-component';
 import { Grafico } from "../../ComponentCompartilhado/grafico/grafico";
 import { CommonModule } from '@angular/common';
+import { MenuLateralComponent } from '../menu-lateral-component/menu-lateral-component';
 import { Aside } from '../aside/aside';
 
 @Component({
   selector: 'app-tela-incial-component',
   standalone: true,
-  imports: [CabecalhoComponent, CommonModule, Grafico, Aside],
+  imports: [CabecalhoComponent, CommonModule, Grafico, MenuLateralComponent, Aside],
   templateUrl: './tela-incial-component.html',
   styleUrl: './tela-incial-component.css'
 })
